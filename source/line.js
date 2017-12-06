1ed592fcb85d454c1214582246e809478f7682f2
pepya2 <johnprogrammertz@gmail.com>
2017-12-06 13:36:15 +0300

    Update line.js

--- source/line.js
+++ source/line.js
@@ -1,6 +1,7 @@
-import { isEquals, getTransform } from './helper'
-import React, { PropTypes, Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { isEquals, getTransform } from './helper';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
 
 export default class Line extends Component {
     constructor(props) {
